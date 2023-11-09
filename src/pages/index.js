831d4ee69b55b220daa862e68f620244f2151af9
Max Busboom <max@busboom.org>
2023-11-08 21:59:15 -0700

    max's webpage button

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,10 @@ export default function Home() {
             <h3> Nikki's Page&rarr;</h3>
             <p>Click here ...</p>
           </a>
-
+          <a href='MaxBusboom.js' className='card'>
+            <h3> Nikki's Page&rarr;</h3>
+            <p>Click here ...</p>
+          </a>
           <a
             href='https://github.com/vedant29/Project-CSCI-1000'
             className='card'>
