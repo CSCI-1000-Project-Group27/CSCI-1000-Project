e37fa86074b1198e33b0ea36651bec9ab3638570
Alexander-Baker-1 <143625014+Alexander-Baker-1@users.noreply.github.com>
2023-11-08 21:14:03 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -220,4 +220,4 @@ export default function Home() {
   );
 }
 
-<!--https://app.netlify.com/sites/clinquant-gumption-a9cb53/deploys/654c5a314131302be5f0a0a3-->
+<!--Alex's file https://app.netlify.com/sites/clinquant-gumption-a9cb53/deploys/654c5a314131302be5f0a0a3-->
