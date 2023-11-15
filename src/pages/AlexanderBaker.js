a7ec7507793b8153aac7037d038b77a82e1d14de
Alexander-Baker-1 <143625014+Alexander-Baker-1@users.noreply.github.com>
2023-11-15 14:45:14 -0700

    Update AlexanderBaker.js

--- src/pages/AlexanderBaker.js
+++ src/pages/AlexanderBaker.js
@@ -10,7 +10,7 @@ export default function examplePage() {
           </div>
           <img
             className='pic'
-            src='https://imgur.com/a/4tYSUyx'
+            src='<blockquote class="imgur-embed-pub" lang="en" data-id="a/4tYSUyx" data-context="false" ><a href="//imgur.com/a/4tYSUyx"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
             alt='alexander baker'
           />
 
